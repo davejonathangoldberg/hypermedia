d2456ed9fbacdd94c6f7b3bc39306ba0cef50994
Dave Goldberg <your_email@youremail.com>
2013-12-29 13:36:27 -0500

    added mongo

--- Database.js
+++ Database.js
@@ -1,6 +1,10 @@
 // Database.js
 module.exports = function Database(configuration) {
   var mongoskin = require('mongoskin');
-  var db = mongoskin.db(configuration.host, {database:configuration.database,safe:configuration.safe});
+  var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'configuration.host';
+  
+  var db = mongoskin.db(mongoUri, {database:configuration.database,safe:configuration.safe});
   return db;
 };
