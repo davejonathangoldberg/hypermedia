545a9bca267ec399e43c2a96652c8a95e7386ac2
Dave Goldberg <your_email@youremail.com>
2013-12-29 13:43:07 -0500

    fixed minor mongo config error

--- Database.js
+++ Database.js
@@ -3,7 +3,7 @@ module.exports = function Database(configuration) {
   var mongoskin = require('mongoskin');
   var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
-  'configuration.host';
+  configuration.host;
   
   var db = mongoskin.db(mongoUri, {database:configuration.database,safe:configuration.safe});
   return db;
